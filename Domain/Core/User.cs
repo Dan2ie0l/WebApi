2f683907d1ea3ec8386bcd296c6a75ad30f6faa1
MaviRamsay <36304554+MaviRamsay@users.noreply.github.com>
2021-05-28 01:20:01 +0400

    Update User.cs

--- Domain/Core/User.cs
+++ Domain/Core/User.cs
@@ -6,14 +6,6 @@ namespace RestApi.Domain.Core
 {
     public class User : IdentityUser<Guid>
     {
-        public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
-        public string Role { get; set; }
-        public string Password { get; set; }
-
 
     }
 }
