6c3894527c47cd986a4efa21fafd7e1d650a5aa1
MaviRamsey <race_1910@mail.ru>
2021-06-04 23:35:38 +0400

    Reservation service created

---
+++ Services/Implementations/ReservationService.cs
@@ -0,0 +1,52 @@
+﻿using RestApi.Domain.Core;
+using RestApi.Domain.Interfaces;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestApi.Services.Implementations
+{
+    public class ReservationService : IReservationService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ReservationService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public async Task CreateAsync(Reservation reservation)
+        {
+            await unitOfWork.Reservations.InsertAsync(reservation);
+            await unitOfWork.CommitAsync();
+        }
+
+        public async Task DeleteAsync(Reservation reservation)
+        {
+            unitOfWork.Reservations.Delete(reservation);
+            await unitOfWork.CommitAsync();
+        }
+
+        public async Task<IEnumerable<Reservation>> GetAllAsync(Expression<Func<Reservation, bool>> expression = null)
+        {
+            return await unitOfWork.Reservations.GetAllAsync();
+        }
+
+        public async Task<Reservation> GetByIdAsync(int id)
+        {
+            return await unitOfWork.Reservations.GetByIdAsync(id);
+        }
+
+        public async Task UpdateAsync(Reservation reservation)
+        {
+            if (!(reservation is null))
+            {
+                unitOfWork.Reservations.Update(reservation);
+                await unitOfWork.CommitAsync();
+            }
+        }
+    }
+}
---
+++ Services/Interfaces/IReservationService.cs
@@ -0,0 +1,46 @@
+﻿using RestApi.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IReservationService
+    {
+        /// <summary>
+        /// Gets Reservation record from Database by ID
+        /// </summary>
+        /// <param name="id">Unique identifier for Reservation</param>
+        /// <returns>Reservation instance, if it exists, else null</returns>
+        Task<Reservation> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Gets all Reservations by condition
+        /// </summary>
+        /// <param name="expression">Condition (lambda)</param>
+        /// <returns>Collection of Reservation instances, if at least 1 Reservation meets the conditions, else null</returns>
+        Task<IEnumerable<Reservation>> GetAllAsync(Expression<Func<Reservation, bool>> expression = null);
+
+        /// <summary>
+        /// Create a new Reservation record in Database
+        /// </summary>
+        /// <param name="Reservation">Reservation data instance</param>
+        /// <returns></returns>
+        Task CreateAsync(Reservation Reservation);
+
+        /// <summary>
+        /// Delete Reservation record from Database
+        /// </summary>
+        /// <param name="Reservation">Reservation data instance</param>
+        /// <returns></returns>
+        Task DeleteAsync(Reservation Reservation);
+
+        /// <summary>
+        /// Update an already existing Reservation record
+        /// </summary>
+        /// <param name="Reservation">Reservation new instance</param>
+        /// <returns></returns>
+        Task UpdateAsync(Reservation Reservation);
+    }
+}
